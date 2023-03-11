00000050f2f89a450916bede6a9fe5cdb3685516
Mikhail Dunaev <mdd375@yandex.ru>
2023-03-11 08:28:12 +0400

    Сигнатура функций приведена к требуемому виду

--- ඞquicksortඞ/parallel.hpp
+++ ඞquicksortඞ/parallel.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <thread>
 #include "unparallel.hpp"
+#include <thread>
 
 template <class RandomIt, class SchemePolicy>
 void pquicksort(RandomIt first, RandomIt last, SchemePolicy &&partition) {
@@ -16,6 +16,23 @@ void pquicksort(RandomIt first, RandomIt last, SchemePolicy &&partition) {
     auto t2 = std::thread(
         [last, pivot, partition] { pquicksort(pivot, last, partition); });
     t1.join();
-    t2.join(); 
+    t2.join();
+  }
+}
+
+template <class RandomIt, class Compare, class SchemePolicy>
+void pquicksort(RandomIt first, RandomIt last, Compare comp, SchemePolicy &&partition) {
+  if (first < last) {
+    if (last - first < 1'000'000) {
+      quicksort(first, last, partition);
+      return;
+    }
+    RandomIt pivot = partition(first, last);
+    auto t1 = std::thread(
+        [first, pivot, comp, partition] { pquicksort(first, pivot, comp, partition); });
+    auto t2 = std::thread(
+        [last, pivot, comp, partition] { pquicksort(pivot, last, comp, partition); });
+    t1.join();
+    t2.join();
   }
 }
--- ඞquicksortඞ/unparallel.hpp
+++ ඞquicksortඞ/unparallel.hpp
@@ -1,9 +1,19 @@
 #pragma once
+
 template <class RandomIt, class SchemePolicy>
 void quicksort(RandomIt first, RandomIt last, SchemePolicy &&partition) {
   if (last - first > 1) {
     RandomIt pivot = partition(first, last);
     quicksort(first, pivot, partition);
     quicksort(pivot, last, partition);
+  }
+}
+
+template <class RandomIt, class Compare, class SchemePolicy>
+void quicksort(RandomIt first, RandomIt last, Compare comp, SchemePolicy &&partition) {
+  if (last - first > 1) {
+    RandomIt pivot = partition(first, last, comp);
+    quicksort(first, pivot, comp, partition);
+    quicksort(pivot, last, comp, partition);
   }
 }
